44dd3d22bbbf54622cef3d2a82268bb84936468b
karuna Chimankar <sukhad.bhole@gmail.com>
2023-02-02 14:39:13 +0000

    Added first test scenarios

---
+++ src/main/java/scenarios/PromotionEngine.java
@@ -0,0 +1,10 @@
+package scenarios;
+
+public class PromotionEngine {
+
+	public int promote(Cart cart) {
+		// TODO Auto-generated method stub
+		return 100;
+	}
+
+}
--- src/test/java/scenarios/PromotionEngineTest.java
+++ src/test/java/scenarios/PromotionEngineTest.java
@@ -1,9 +1,23 @@
 package scenarios;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class PromotionEngineTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Promotion Engine");
+	}
+	
+	@Test
+	public void scenarioA() {
+		PromotionEngine promotionEngine = new PromotionEngine();
+		
+		Cart cart = new Cart();
+		
+		Assert.assertEquals(100, promotionEngine.promote(cart));
+		
 	}
 
 }
