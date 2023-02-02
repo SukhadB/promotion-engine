812ba51ee8b69d0eb1f680cff30789f8ba0025a5
karuna Chimankar <sukhad.bhole@gmail.com>
2023-02-02 15:00:10 +0000

    Added Test Scenario to add product to cart and create product and refracted code accordingly

--- src/main/java/scenarios/Cart.java
+++ src/main/java/scenarios/Cart.java
@@ -1,9 +1,22 @@
 package scenarios;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
 	
 	private List<Product> productList;
+	
+	public int calculateTotalCartValue() {
+		int totalCost = 0;
+		return totalCost;
+	}
+
+	public void addProduct(Product product) {
+		if(this.productList == null) {
+			this.productList = new ArrayList<Product>();
+		}
+		this.productList.add(product);		
+	}
 
 }
--- src/test/java/scenarios/PromotionEngineTest.java
+++ src/test/java/scenarios/PromotionEngineTest.java
@@ -11,13 +11,25 @@ public class PromotionEngineTest {
 	}
 	
 	@Test
-	public void scenarioA() {
+	public void promote() {
 		PromotionEngine promotionEngine = new PromotionEngine();
 		
 		Cart cart = new Cart();
-		
+				
 		Assert.assertEquals(100, promotionEngine.promote(cart));
 		
 	}
 
+	@Test
+	public void addProductToCart() {
+		PromotionEngine promotionEngine = new PromotionEngine();
+		
+		Cart cart = new Cart();
+		
+		Product product = new Product("A", 50);
+		cart.addProduct(product);
+		
+		Assert.assertEquals(100, promotionEngine.promote(cart));
+		
+	}
 }
