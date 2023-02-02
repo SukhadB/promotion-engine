55a887aa7e3008203542420bbc14f76267c45c11
karuna Chimankar <sukhad.bhole@gmail.com>
2023-02-02 17:22:49 +0000

    Added getter setter for total amount in cart

--- src/main/java/scenarios/Cart.java
+++ src/main/java/scenarios/Cart.java
@@ -8,6 +8,14 @@ public class Cart {
 	private List<Product> productList;
 	private double totalAmount;
 	
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
 	public List<Product> getProductList() {
 		return productList;
 	}
