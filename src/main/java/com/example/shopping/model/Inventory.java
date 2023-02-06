b8fda7d30562c385418fb774edac89acc95e0caa
karuna Chimankar <sukhad.bhole@gmail.com>
2023-02-06 16:59:09 +0000

    Added class inventory which will consists all products and promotion details

--- src/main/java/com/example/shopping/model/Inventory.java
+++ src/main/java/com/example/shopping/model/Inventory.java
@@ -4,8 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Inventory {
+
+	private Map<String, Product> listedProducts = new HashMap<>();
+	private Map<String, PromotionConfiguration> promotions = new HashMap<>();
 	
-	private Map<String, Product> listedItems = new HashMap<>();
-	private Map<String, PromotionConfiguration> allPromotions = new HashMap<>();
+	public Inventory(Map<String, Product> listedProducts, Map<String, PromotionConfiguration> promotions) {
+		this.listedProducts = listedProducts;
+		this.promotions = promotions;
+	}
+
+	public Map<String, Product> getListedProducts() {
+		return listedProducts;
+	}
+
+	public Map<String, PromotionConfiguration> getPromotions() {
+		return promotions;
+	}
 
 }
