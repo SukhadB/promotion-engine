e62b89b9469cf92f84f3d8b0beef0cb0a823e924
sukhadbhole <sukhadbhole@192.168.1.251>
2023-02-13 18:58:11 +0000

    New test cases added

--- src/test/java/com/example/shopping/processor/PercentagePromoProcessorTest.java
+++ src/test/java/com/example/shopping/processor/PercentagePromoProcessorTest.java
@@ -79,5 +79,22 @@ public class PercentagePromoProcessorTest {
 		PercentagePromoProcessor promoProcessor = new PercentagePromoProcessor();
 		assertEquals(promoProcessor.applyPromotion(products, promotion), 120.0d, 0.001);
 	}
+	
+	@Test
+	public void testApplyPromotionMultipleCombination3() throws ProductNotPresentInInvetory {
+		List<Product> products = new ArrayList<>();
+		Map<String, Integer> promoDetails1 = new HashMap<>();
+		promoDetails1.put("B", 2);
+		Promotion promotion = new Promotion("P2", promoDetails1, 0, 80);
+		Product b = new Product("B", 30);
+		products.add(b);
+		products.add(b);
+		products.add(b);
+		products.add(b);
+		products.add(b);
+		products.add(b);
+		PercentagePromoProcessor promoProcessor = new PercentagePromoProcessor();
+		assertEquals(promoProcessor.applyPromotion(products, promotion), 144.0d, 0.001);
+	}
 
 }
